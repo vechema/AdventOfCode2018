d6d20afc235f927d8656e3cb970e6542f8c4a589
Jo Egner <vechema@gmail.com>
2018-12-25 09:44:05 -0600

    Day 2, part 2

--- 2/AoCDay2.js
+++ 2/AoCDay2.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 var input = fs.readFileSync('input.txt', 'utf8')
 var inputs = input.split("\r\n");
 
-console.log(inputs)
+//console.log(inputs)
 
 var lettersTwice = 0;
 var lettersThrice = 0;
@@ -40,3 +40,22 @@ for(var i = 0; i < inputs.length; i++) {
 }
 
 console.log(lettersTwice * lettersThrice)
+
+for(var i = 0; i < inputs.length; i++) {
+    for(var j = i+1; j < inputs.length; j++) {
+        let word1 = inputs[i];
+        let word2 = inputs[j];
+        let numDiff = 0;
+        for (var k = 0; k < word1.length; k++) {
+            if(word1.charAt(k) != word2.charAt(k)) {
+                ++numDiff;
+            }
+            if (numDiff > 1) {
+                break;
+            }
+        }
+        if(numDiff == 1) {
+            console.log(word1 + " " + word2)
+        }
+    }
+}
